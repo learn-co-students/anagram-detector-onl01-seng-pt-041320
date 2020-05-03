0116f20b2bd9248da2a93fd4fca63c4d5a77b1df
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-05-03 18:47:49 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    attr_accessor :word
+
+
+    def initialize(word)
+        @word = word.split("").sort
+    end
+
+    def match(words)
+        racecar = []
+        words.collect do |sword|
+            abc = sword.split("").sort
+            # binding.pry
+            if abc == word
+                racecar << sword
+            end
+        end
+        racecar
+    end
+
+end
