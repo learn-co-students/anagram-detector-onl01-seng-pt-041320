9873e524431b91da770a73143ed71b5a93d8d3cf
Patricia Cordero <patricia@patriciaco.com>
2020-05-04 16:47:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  def match(words)
+    #if no anagram match, return []
+    #iterate over words_arr checking if each word contains the same letters and self.word
+    words.select do |word|
+      #comparison between 'word' and self.word(@word) - do they contain the same letters?
+      # "ba" => "ab"
+        if word.length != self.word.length
+          next
+        end
+        self.word.split("").sort == word.split("").sort
+    end
+  end
+end
+
+#bonus points for anyone that can figure out how to solve without using .sort
