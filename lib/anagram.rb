ddf0c6cf599cec6dee863480c16f0651e17836d5
OrkunSA <c.orkun.s@gmail.com>
2020-05-05 20:02:35 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+    
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words_of_array)
+        words_of_array.select do |word|
+        #I am using select method here because I need to compare each word
+        #of my array to the word that the anagram class is initialized with
+        word.split("").sort == @word.split("").sort
+        # we need to iterate over the words_of_array and comparate 
+        # each word in that array to to the word
+        #our Anagram class took upon initialization
+        #we can split a word into an array of letter by using .split method
+        #and in order to compare if they contain the same letters or not
+        #we use .sort method
+        end
+    end
+end
+
