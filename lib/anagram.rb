a5eb7ea1db027a93318e7a69baf12e0d9247453d
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-06 17:43:48 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :anagram
+    def initialize(anagram)
+        @anagram = anagram
+    end
+
+    def is_anagram?(word)
+        word.chars.sort == @anagram.chars.sort
+    end
+
+    def match(words)
+        words.select do |word|
+            is_anagram?(word)
+        end
+    end
+end
