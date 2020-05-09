6b21063705e31ed4bfa1e130ebe444172f050f2a
Philippe Timothe <philippetimothe@gmail.com>
2020-05-09 12:43:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(words)
+    matches = []
+    #if no anagram match, return[]
+    #iterate over words_arr checking if each word contains the same letters and self.word 
+    words.select do |word|
+      if word.length != self.word.length 
+        next
+      end
+      if self.word.split("").sort == word.split("").sort
+        matches << word
+      end
+    end
+    matches
+  end
+  
+end
