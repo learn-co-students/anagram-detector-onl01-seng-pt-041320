3b70e323e7c72be0e1377d0edf2e07f725ccad1d
Joseph Romero <joeromero47@gmail.com>
2020-05-10 03:24:27 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def match(arr)
+    arr.keep_if {|str| name.split('').sort == str.split('').sort}
+  end
+end
